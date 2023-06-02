7431f6574de1165d84c5318061ad8e479be3488a
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-06-02 14:46:00 -0400

    added cm to inch and inch to cm

--- convert.kt
+++ convert.kt
@@ -4,6 +4,8 @@ fun main() {
     println("Available conversions:")
     println("1. Kilometers to Miles")
     println("2. Miles to Kilometers")
+    println("3. Centimeters to Inches")
+    println("4. Inches to Centimeters")
  
 
     print("Enter the numeric value: ")
@@ -21,6 +23,8 @@ fun main() {
         /*function call to Km to miles*/
         1 -> convertKilometersToMiles(value)
         2 -> convertMilesToKilometers(value)
+        3 -> convertCentimetersToInches(value)
+        4 -> convertInchesToCentimeters(value)
  
         else -> {
             println("Invalid unit of measurement. Exiting the program.")
@@ -38,4 +42,11 @@ fun convertKilometersToMiles(kilometers: Double): Double {
 /* FUNCTION FOR miles to kilometer */
 fun convertMilesToKilometers(miles: Double): Double {
     return miles * 1.61
+}
+fun convertCentimetersToInches(centimeters: Double): Double {
+    return centimeters * 0.39
+}
+
+fun convertInchesToCentimeters(inches: Double): Double {
+    return inches * 2.54
 }
